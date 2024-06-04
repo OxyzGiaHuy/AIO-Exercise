ad10ae28cd67f0bc0a848dbb7d3807c307267188
OxyzGiaHuy <108162560+OxyzGiaHuy@users.noreply.github.com>
2024-06-05 01:12:17 +0700

    Update approximation.py

--- Module_1/Week_1/approximation.py
+++ Module_1/Week_1/approximation.py
@@ -36,11 +36,14 @@ print(round(approx_cos(x=3.14 , n =10) , 2))
 # Trac nghiem cau 10
 assert round(approx_sin(x=1, n =10), 4) == 0.8415
 print(round(approx_sin(x=3.14, n =10) , 4))
+# Output: 0.0016
 
 # Trac nghiem cau 11
 assert round(approx_sinh(x = 1, n = 10), 2) == 1.18
 print(round(approx_sinh(x = 3.14, n = 10), 2))
+# Output: 11.53
 
 # Trac nghiem cau 12
 assert round(approx_cosh(x = 1, n = 10), 2) == 1.54
 print(round(approx_cosh(x = 3.14, n = 10), 2))
+# Output: 11.57
