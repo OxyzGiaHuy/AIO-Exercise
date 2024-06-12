6c07785e085d769b11ea67040c229dc8e3514971
ThaiGiaHuy <thaigiahuy.hlk@gmail.com>
2024-06-13 06:51:41 +0700

    add sonarlint and fix pep8

--- Module_1/Week_1/approximation.py
+++ Module_1/Week_1/approximation.py
@@ -4,11 +4,13 @@ def factorial(n):
         factor *= i
     return factor
 
+
 def approx_sin(x, n):
     sin = 0
     for i in range(0, n):
         sin += ((-1)**i) * (x**(2*i+1)) / factorial(2*i+1)
     return sin
+
 
 def approx_cos(x, n):
     cos = 0
@@ -16,31 +18,16 @@ def approx_cos(x, n):
         cos += ((-1)**i) * (x**(2*i)) / factorial(2*i)
     return cos
 
+
 def approx_sinh(x, n):
     approx_sinh = 0
     for i in range(0, n):
         approx_sinh += (x**(2*i+1)) / factorial(2*i+1)
     return approx_sinh
 
+
 def approx_cosh(x, n):
     cosh = 0
     for i in range(0, n):
         cosh += (x**(2*i)) / factorial(2*i)
     return cosh
-
-# Trac nghiem cau 9
-assert round(approx_cos(x=1, n =10), 2) == 0.54
-print(round(approx_cos(x=3.14 , n =10) , 2))
-# Output: -1.0
-
-# Trac nghiem cau 10
-assert round(approx_sin(x=1, n =10), 4) == 0.8415
-print(round(approx_sin(x=3.14, n =10) , 4))
-
-# Trac nghiem cau 11
-assert round(approx_sinh(x = 1, n = 10), 2) == 1.18
-print(round(approx_sinh(x = 3.14, n = 10), 2))
-
-# Trac nghiem cau 12
-assert round(approx_cosh(x = 1, n = 10), 2) == 1.54
-print(round(approx_cosh(x = 3.14, n = 10), 2))
